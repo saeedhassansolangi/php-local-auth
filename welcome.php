9a76d7822593577579b79326f696b81ab70ba96b
saeedhassansolangi <saeedhassansolangi@gmail.com>
2022-03-15 17:43:57 +0500

    updated files

--- welcome.php
+++ welcome.php
@@ -10,17 +10,11 @@ if($_SESSION['username'] == ""){
 ?>
 
 <div class="welcome">
-
-
     <div id="welcome-head">
-        <img src="https://avatars.githubusercontent.com/u/45068032?v=4" class="profile" alt="">
         <h1> Welcome back  <span class="user"><?php echo $_SESSION['username']?></span></h1>
     </div>
 
     <p class="logout"><a href="./logout.php">Logut</a></p>
-
-
-
 </div>
 <?php
 require ('./partials/footer.php');
